c88ae33653a17d18c5c26c40614677b649d2df09
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-08 19:04:30 +0000

    sdfff

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { cors } from 'cors';
 
+// Use this after the variable declaration
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(3000);
+  app.use(cors());
 }
 bootstrap();
