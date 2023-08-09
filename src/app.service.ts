3efcecd9d0125030ded711de75a7a9296a4c0442
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-09 16:29:58 +0000

    dfgg

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-export const filePath = path.join(process.cwd(), './products.json');
+export const filePath = path.join(process.cwd(), './public/products.json');
 @Injectable()
 export class UsersAppService {
   getUsers(): string[] {
