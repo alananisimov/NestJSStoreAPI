e01ab3cc9f66e17f3d880b798198ecc56044132e
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-10 18:51:52 +0000

    sff

--- src/products.service.ts
+++ src/products.service.ts
@@ -43,18 +43,40 @@ export class ProductsService {
   }
 
   async getAllProducts() {
-    this.loadProducts();
-    return this.products;
+    const req = axios.default.get(
+      'https://edge-config.vercel.com/ecfg_jeulv3pkm9h0aj04qaufb2fgqxbf/item/products?token=4679659a-ad8c-4aa9-92e7-8345465955d0',
+    );
+    return req;
   }
 
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-    this.saveProducts();
-    return this.products;
+  async addProduct(newProduct: Product) {
+    const res = await axios.default.patch(
+      'https://api.vercel.com/v1/edge-config/ecfg_jeulv3pkm9h0aj04qaufb2fgqxbf/items',
+      {
+        items: [{ operation: 'update', key: 'products', value: newProduct }],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer 7KDKt8lR35SJHgPfcRhJrPT1',
+        },
+      },
+    );
+    console.log(res);
   }
-  deleteProduct() {
-    this.products = [];
-    this.saveProducts();
-    return this.products;
+  async deleteProduct() {
+    const res = await axios.default.patch(
+      'https://api.vercel.com/v1/edge-config/ecfg_jeulv3pkm9h0aj04qaufb2fgqxbf/items',
+      {
+        items: [{ operation: 'update', key: 'products', value: [] }],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer 7KDKt8lR35SJHgPfcRhJrPT1',
+        },
+      },
+    );
+    console.log(res);
   }
 }
