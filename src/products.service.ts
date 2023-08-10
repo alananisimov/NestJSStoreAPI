5553086f61da96cff067bf864b3f1f8d0a3d1f88
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-10 18:52:20 +0000

    sdff

--- src/products.service.ts
+++ src/products.service.ts
@@ -4,44 +4,6 @@ import { get } from '@vercel/edge-config';
 import * as axios from 'axios';
 @Injectable()
 export class ProductsService {
-  private products: Product[] = [];
-
-  constructor() {
-    this.loadProducts();
-  }
-
-  private async loadProducts() {
-    try {
-      const data: Array<Product> = await get('products');
-      this.products = data;
-      console.log(this.products);
-    } catch (error) {
-      console.error('Error loading products:', error);
-    }
-  }
-
-  private async saveProducts() {
-    try {
-      const res = await axios.default.patch(
-        'https://api.vercel.com/v1/edge-config/ecfg_jeulv3pkm9h0aj04qaufb2fgqxbf/items',
-        {
-          items: [
-            { operation: 'update', key: 'products', value: this.products },
-          ],
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer 7KDKt8lR35SJHgPfcRhJrPT1',
-          },
-        },
-      );
-      console.log(res);
-    } catch (err) {
-      console.log(err.response);
-    }
-  }
-
   async getAllProducts() {
     const req = axios.default.get(
       'https://edge-config.vercel.com/ecfg_jeulv3pkm9h0aj04qaufb2fgqxbf/item/products?token=4679659a-ad8c-4aa9-92e7-8345465955d0',
