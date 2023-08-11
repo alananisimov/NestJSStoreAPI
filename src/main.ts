039322af02805d64ab72f487b35e8b1b37809686
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-11 16:57:57 +0000

    dfgg

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     root: path.join(__dirname, 'public'),
     prefix: '/public/',
   });
-  await app.listen(3000);
+  await app.listen(process.env.port || 3000);
 }
 bootstrap();
