04dcbb039703eb740e90b80541dac2adc6876a2e
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-08-11 17:03:41 +0000

    df

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import * as path from 'path';
-
+const port = process.env.PORT || 3000;
 // Use this after the variable declaration
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,6 +16,6 @@ async function bootstrap() {
     root: path.join(__dirname, 'public'),
     prefix: '/public/',
   });
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
