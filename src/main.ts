5968c427494d2450f3c8033cd24a5dd75ef641fe
alananisimov <alananisimov@gmail.com>
2023-11-27 22:44:22 +0300

    123

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { cors: false },
   );
   // const { rows, fields } = await db.sql`SELECT * FROM reviews`;
   // console.log(rows, fields);
   app.useStaticAssets({
     root: path.join(process.cwd(), './public'),
   });
-  app.enableCors();
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
